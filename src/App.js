96614fac16f24e061e3eac082310973f2328d28b
unknown <cogitaciones1@gmail.com>
2024-03-08 18:16:24 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import PlayWithLetter from "./Pages/PlayWithLetters";
@@ -8,14 +8,14 @@ import Navbar from "./Componets/Navbar";
 import PlayWithNumbers from "./Pages/PlayWithNumbers";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/play-with-letters" element={<PlayWithLetter />} />
         <Route exact path="/play-with-numbers" element={<PlayWithNumbers />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
